007147d55e78aa718bc8ee44711ea7a7bf677fd9
Priscila Silva <priscila.rodrigues@hotmail.com.br>
2020-12-26 09:32:02 +0100

    Implement authentication using JWT

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,12 +1,22 @@
 const express = require("express");
+const auth = require("./auth");
 
 // receber o server de server.js
 module.exports = function (server) {
-  // Definir URL base para todas as rotas
-  const router = express.Router();
-  server.use("/api", router);
-
-  // Rotas de Ciclo de Pagamento
+  // Define URL base para rota protegida com middleware auth JWT
+  const protectedApi = express.Router();
+  server.use("/api", protectedApi);
+  protectedApi.use(auth);
+  // Rotas de Ciclo de Pagamento com auth JWT
   const BillingCycle = require("../api/billingCycle/billingCycleService");
-  BillingCycle.register(router, "/billingCycles");
+  BillingCycle.register(protectedApi, "/billingCycles");
+
+  // API aberta
+  const openApi = express.Router();
+  server.use("/oapi", openApi);
+  // Rotas abertas
+  const AuthService = require("../api/user/authService");
+  openApi.post("/login", AuthService.login);
+  openApi.post("/signUp", AuthService.signUp);
+  openApi.post("/validateToken", AuthService.validateToken);
 };
